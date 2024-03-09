416ef0ba4904081986f05736bb88c3e70d599f38
Sam-ryong <zackinthebox@khu.ac.kr>
2024-03-09 21:01:17 +0900

    hello

---
+++ HelloWorld/HelloWorldApp.java
@@ -0,0 +1,9 @@
+public class HelloWorldApp {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
+// 파일명과 class 이름이 같아야함
+// 실행을 시키면 class 안의 main 함수를 실행함
+// .java 파일을 컴파일 -> .class 파일
+// .class 파일은 실행파일임.
